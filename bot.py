785906755fdf71a4858034ccd9f113bb70d22058
msrogers2015 <69293041+msrogers2015@users.noreply.github.com>
2021-02-06 22:21:39 -0500

    Code Rework
    -Re coded the entire bot (with some copy and paste)
    -Updated holiday dicts to include only month and date.
    -Fixed the year change-over bug
    -Embed color update(to be updated based on reletive holidays)
    -Prefix changed to h-
    -Server count is self updating without needing to restart the bot
    -Time zone changed to UTC +0
    -Commands changed from isit to is and whenis to when.

--- bot.py
+++ bot.py
@@ -1,148 +1,185 @@
-#Import files for use
+# IsIt Bot
+
+# Imports
 import os
-import random
 import discord
-import datetime
-
-
-#Import specific items from above files
+import datetime as dt
+# Specific imports
 from dotenv import load_dotenv
 from discord.ext import commands
 
-#Setting up bot token
+
+# Setting up the bot
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
-
-bot = commands.Bot(command_prefix='-', activity=discord.Activity(type=discord.ActivityType.watching,
-       name=str(len(bot.guilds)) + " servers | -help"))
+bot = commands.Bot(command_prefix='h-')
 bot.remove_command('help')
 
 
-# Get next year
-y = datetime.datetime.now()
+# Variables
+# Colors
+valentines =0xdf6979
+embed_color = valentines
+timezone = 'Based on UTC +0 time'
+github = 'https://github.com/msrogers2015/IsItBot'
 
-# Holidays
 holidays = {
-    'christmas' : datetime.datetime(y.year, 12, 25),
-    'new year' : datetime.datetime(y.year, 1, 1),
-    'valentines' : datetime.datetime(y.year, 2, 14),
-    'aprilfools' : datetime.datetime(y.year, 4, 1),
-    'cincodemayo' : datetime.datetime(y.year, 5, 5),
-    'juneteenth' : datetime.datetime(y.year, 6, 19),
-    'halloween' : datetime.datetime(y.year, 10, 31),
-    'veterns' : datetime.datetime(y.year, 11, 11),
-    'independence': datetime.datetime(y.year, 7, 4),
+    "new year" : [1,1],
+    'valentines' : [2,14],
+    'april fools' : [4,1],
+    'cinco de mayo' : [5,5],
+    'juneteenth' : [6,19],
+    'independence' : [7,1],
+    'halloween' : [10,31],
+    'veterns' : [11,11],
+    'christmas' : [12,25],
 }
 
-'''
-# Holidays that change dates
-easter
-mothersday
-fathersday
-thanksgiving
-springforward
-fallback
-memroial
-winter
-summer
-laborday
-blackfriday
-cybermonday
-'''
 
-#Embed Color
-embed_color = 0xff8200
-timezone = "Based on Louisville, KY UTC-5"
-
-
-
-#When the bot is online, print in terminal
+# Print that the bot is online
 @bot.event
 async def on_ready():
-   print(f'{bot.user.name} has connected to a server')
+    print(f'{bot.user.name} is online.')
+    await bot.change_presence(
+       activity=discord.Activity(type=discord.ActivityType.watching,
+       name=str(len(bot.guilds)) + " servers | h-help")
+    )
 
 
-#Ping command to check for latency
+# Update server count
+async def on_guild_join():
+    await bot.change_presence(
+       activity=discord.Activity(type=discord.ActivityType.watching,
+       name=str(len(bot.guilds)) + " servers | h-help")
+    )
+async def on_guild_remove():
+    await bot.change_presence(
+       activity=discord.Activity(type=discord.ActivityType.watching,
+       name=str(len(bot.guilds)) + " servers | h-help")
+    )
+
+
+# Basic latency test
 @bot.command()
 async def ping(ctx):
     embed = discord.Embed(title="Pong!", description=f'{round(bot.latency*1000)}ms', color=embed_color)
-    embed.set_footer(text=timezone)
     await ctx.send(embed=embed)
 
 
+# Check if today is requested holiday
+@bot.command(aliases=['is'])
+async def _is(ctx, *, holiday):
+    # If today month and day is the same as the saved value
+    if holiday in holidays:
+        # Creating date objects
+        current = dt.datetime.utcnow()
+        date = holidays[holiday]
+        d = dt.datetime(current.year, date[0], date[1])
+        # If date matches today
+        if d.month and d.date == current.month and current.day:
+            embed = discord.Embed(title=f'Is it {holiday.title()}?', description=f'Yes! Today is {holiday.capitalize()}. :smile: ', color=embed_color)
+            embed.set_footer(text=timezone)
+            embed.add_field(name=holiday.capitalize(), value=d)
+            await ctx.send(embed=embed)
+        # If date doesn't match today
+        else:
+            embed = discord.Embed(title=f'Is it {holiday.title()}?', description=f'Sorry, it is\'t {holiday.capitalize()} :cry:', color=embed_color)
+            embed.set_footer(text=timezone)
+            embed.add_field(name=holiday.capitalize(), value=d)
+            await ctx.send(embed=embed)
+    # If holiday isn't in the list
+    if holiday not in holidays:
+        embed = discord.Embed(title="Error 404: Holiday not found", description=f'Sorry, this holiday hasn\'t been added to the last. Try making a suggestion by opening an issue on the github repo <{github}>', color=embed_color)
+        embed.set_footer(text=timezone)
+        await ctx.send(embed=embed)
+
+
+# Giving Wikipedia information for holidays
 @bot.command()
-async def whenis(ctx, *,holiday):
+async def wiki(ctx, *, holiday):
+    if holiday in holidays:
+        # Parsing
+        uppercase = holiday.title()
+        search = holiday.replace(' ',"_")
+        await ctx.send(f'<https://en.wikipedia.org/wiki/{search}>')
+    if holiday not in holidays:
+        embed = discord.Embed(title="Error 404: Holiday not found", description=f'Sorry, this holiday hasn\'t been added to the last. Try making a suggestion by opening an issue on the github repo <{github}>', color=embed_color)
+        embed.set_footer(text=timezone)
+        await ctx.send(embed=embed)
 
-    # Get the current year and check for leap year
-    y = datetime.datetime.now()
-    if y.year % 4 ==0:
-        year_add = 366
-    else:
-        year_add = 365
+
+# A list of currently accepted holidays
+@bot.command()
+async def dates(ctx):
+    embed = discord.Embed(title='Holidays', description='A list of currently supported holidays', color=embed_color)
+    for date in holidays:
+        d = holidays[date]
+        embed.add_field(name=date.title(), value=f'{d[0]}-{d[1]}' ,inline=True)
+    return await ctx.send(embed=embed)
 
 
-    # Check if holiday has been listed
-    if(holiday in holidays):
-        time_diff = holidays[holiday] - y
-        # If the holiday has already passed, add one year
-        if time_diff.total_seconds() < 0:
-            updated = (holidays[holiday]+ datetime.timedelta(days=year_add)) - y
-            result = str(updated)
+# Check time until a holiday
+@bot.command()
+async def when(ctx, *,holiday):
+    if holiday in holidays:
+        # Creating date objects
+        current = dt.datetime.utcnow()
+        date = holidays[holiday]
+        d = dt.datetime(current.year, date[0], date[1])
+        year_offset = 365
+        t_delta = d - current
+        # If date matches today
+        if d.day and d.month == current.day and current.month:
+            embed = discord.Embed(title=f'Today is {holiday.title()}!', description=f'Today is {holiday.capitalize()}. how are you going to celebrate?', color=embed_color)
+            embed.set_footer(text=timezone)
+            embed.add_field(name=holiday, value=d)
+            await ctx.send(embed=embed)
+        # If date has passed
+        elif t_delta.total_seconds() < 0:
+            h_passed = (d + dt.timedelta(days=year_offset)) - current
+            result = str(h_passed)
             info, leftover = result.split('.')
 
             embed = discord.Embed(title=f'When is {holiday.title()}?', description=f'{holiday.capitalize()} is in {info}.', color=embed_color)
             embed.set_footer(text=timezone)
             await ctx.send(embed=embed)
-        # If holiday hasn't passed
+        # If date is in the future
         else:
-            result = str(time_diff)
+            result = str(t_delta)
             info, leftover = result.split('.')
 
             embed = discord.Embed(title=f'When is {holiday.title()}?', description=f'{holiday.capitalize()} is in {info}', color=embed_color)
             embed.set_footer(text=timezone)
             await ctx.send(embed=embed)
-
-
-
-@bot.command()
-async def isit(ctx, *, holiday):
-    if holidays[holiday].month and holidays[holiday].day == y.month and y.day:
-        embed = discord.Embed(title=f'Is it {holiday.title()}?', description=f'Yes! Today is {holiday.capitalize()}. :smile: ', color=embed_color)
-        embed.set_footer(text=timezone)
-        await ctx.send(embed=embed)
-    else:
-        embed = discord.Embed(title=f'Is it {holiday.title()}?', description=f'Sorry, it is\'t {holiday.capitalize()} :cry:', color=embed_color)
-        embed.set_footer(text=timezone)
-        await ctx.send(embed=embed)
-
-
-@bot.command()
-async def wiki(ctx, *, holiday):
-    if holiday in holidays:
-        uppercase = holiday.title()
-        search = holiday.replace(' ',"_")
-        await ctx.send(f'<https://en.wikipedia.org/wiki/{search}>')
+    # If holiday isn't in the list
     if holiday not in holidays:
-        await ctx.send('Sorry, I cant provide more information')
+        embed = discord.Embed(title="Error 404: Holiday not found", description=f'Sorry, this holiday hasn\'t been added to the last. Try making a suggestion by opening an issue on the github repo <{github}>', color=embed_color)
+        embed.set_footer(text=timezone)
+        await ctx.send(embed=embed)
 
 
 @bot.command()
-async def dates(ctx):
-    embed = discord.Embed(title='Holidays', description='A list of currently supported holidays', color=embed_color)
-    for date in holidays:
-        embed.add_field(name=date.title(), value=holidays[date].strftime("%b") + '-' + str(holidays[date].day) ,inline=False)
-    return await ctx.send(embed=embed)
-
-
-
-@bot.command()
+# Custom help command
 async def help(ctx):
-    embed = discord.Embed(title='IsItBot Help', description='Directions on how to use difference commands', color=embed_color, inline=False)
-    embed.add_field(name='isit <holiday>', value='Tells you if the requested holiday is today.', inline=False)
-    embed.add_field(name='whenis <holiday', value='Gives a time value for how long until given holiday.', inline=False)
-    embed.add_field(name='wiki <holiday>', value='Returns a wiki link for more information on given holiday.', inline=False)
-    embed.add_field(name='ping', value='Command to see if the bot is online.', inline=False)
-    embed.add_field(name='dates', value='Returns a list of currently accepted holidays', inline=False)
-
+    embed = discord.Embed(title=f'Some useful commands!', color=embed_color)
+    embed.set_footer(text=timezone)
+    embed.add_field(name='h-ping', value='Standard latency testing. Nothing more to see here.', inline=False)
+    embed.add_field(name='h-is', value='Check if today is a certain holiday.', inline=False)
+    embed.add_field(name='h-when', value='Check how long until a given holiday.', inline=False)
+    embed.add_field(name='wiki', value='Get more information about a given holiday.', inline=False)
+    embed.add_field(name='h-dates', value='Get a list of currently accpeted holidays.', inline=False)
     await ctx.send(embed=embed)
+
+
+@bot.command()
+async def support(ctx):
+    embed = discord.Embed(title='Support Links', description='Support a local developer!', color=embed_color)
+    embed.add_field(name='Discord', value='https://discord.com/invite/quailstudio', inline=False)
+    embed.add_field(name='Fiverr', value='https://www.fiverr.com/users/quail_studio/seller_dashboard', inline=False)
+    embed.add_field(name='Patreon', value='https://www.patreon.com/QuailWare', inline=False)
+    embed.add_field(name='Github', value=github, inline=False)
+    await ctx.send(embed=embed)
+
+    
+# Bot run loop
 bot.run(TOKEN)
